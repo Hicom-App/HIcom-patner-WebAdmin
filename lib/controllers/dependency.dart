b0c0683918f54353fe9cff660cb41497a7f3f4d6
To-Rex <torex.amaki@gmail.com>
2025-01-09 23:25:24 +0500

    first

--- lib/controllers/dependency.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'get_controller.dart';
-
-class DependencyInjection {
-  static void init() {
-    Get.put<GetController>(GetController(), permanent: true);
-  }
-}
